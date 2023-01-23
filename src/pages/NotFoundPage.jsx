bc00f18210129364c9a2a601a60e39eb37031878
Medina <mkazic1@etf.unsa.ba>
2023-01-23 18:32:28 +0100

    Made a not found page and its style in separate style folder and file

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { Typography, Box, Button } from '@mui/material';
+import ROUTES from '../constants/routes';
+import { errorPageStyle, linkStyle } from '../styles/pages/NotFoundPageStyle';
+
+function NotFoundPage() {
+  return (
+    <Box sx={errorPageStyle}>
+      <Typography variant="h1" component="h1" sx={{ color: 'text.info' }}>Oops!</Typography>
+      <Typography variant="h4" component="h4" sx={{ color: 'text.primary' }}>404 - This page could not be found!</Typography>
+      <Link to={`${ROUTES.WELCOME}`} style={linkStyle}>
+        <Button variant="contained" sx={{ marginTop: 4 }}>
+          GO BACK
+        </Button>
+      </Link>
+    </Box>
+  );
+}
+
+export default NotFoundPage;
---
+++ src/styles/pages/NotFoundPageStyle.js
@@ -0,0 +1,10 @@
+export const errorPageStyle = {
+  display: 'flex',
+  width: '100vw',
+  height: '100vh',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export const linkStyle = { textDecoration: 'none' };
