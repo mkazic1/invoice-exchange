4a7a4b9246c631c319d2fe7ec732f030e27bd538
Medina <mkazic1@etf.unsa.ba>
2023-01-23 18:34:18 +0100

    Basic routing set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import ROUTES from './constants/routes';
+import NotFoundPage from './pages/NotFoundPage';
+import WelcomePage from './pages/WelcomePage';
+import Providers from './providers';
+
 function App() {
   return (
-    <div>
-      <header>
-        Invoice Exchange
-      </header>
-    </div>
+    <Providers>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES.WELCOME} element={<WelcomePage />} />
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Providers>
   );
 }
 
