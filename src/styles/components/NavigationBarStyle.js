4836a512ec7ccf057adf9e7a0eeb22a6a68b9b1b
Medina <mkazic1@etf.unsa.ba>
2023-01-23 23:30:55 +0100

    Created navigation bar and its relevant style

---
+++ src/styles/components/NavigationBarStyle.js
@@ -0,0 +1,19 @@
+export const appBarStyle = {
+  bgcolor: 'primary.main',
+};
+
+export const containerStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: 0,
+};
+
+export const linkStyle = { textDecoration: 'none' };
+
+export const cardMediaStyle = { width: '8%', marginRight: '24px' };
+
+export const navigationTitleStyle = {
+  letterSpacing: '0.2rem',
+  color: 'text.secondary',
+  fontWeight: 600,
+};
