e4b0ae245e27c7902906827c7a365e61f0b98a17
Medina <mkazic1@etf.unsa.ba>
2023-01-23 19:06:02 +0100

    Routing set up done, changes may be in the future

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ROUTES from './constants/routes';
+import Providers from './providers';
 import NotFoundPage from './pages/NotFoundPage';
 import WelcomePage from './pages/WelcomePage';
-import Providers from './providers';
+import DashboardPage from './pages/DashboardPage';
+import Invoices from './pages/invoices/Invoices';
+import Sellers from './pages/sellers/Sellers';
+import Customers from './pages/customers/Customers';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Routes>
           <Route path={ROUTES.WELCOME} element={<WelcomePage />} />
           <Route path="/" element={<WelcomePage />} />
+          <Route path={ROUTES.DASHBOARD} element={<DashboardPage />} />
+          <Route path={ROUTES.INVOICES} element={<Invoices />} />
+          <Route path={ROUTES.SELLERS} element={<Sellers />} />
+          <Route path={ROUTES.CUSTOMERS} element={<Customers />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
