435e8344f2dcde5d927a10ebb43405e2bf9b6737
Medina <mkazic1@etf.unsa.ba>
2023-01-25 21:02:37 +0100

    Menu bar component and style configured

---
+++ src/styles/components/MenuBarStyle.js
@@ -0,0 +1,27 @@
+export const menuOptionsStyle = {
+  marginTop: '80px',
+  marginX: '10px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  height: '100%',
+};
+
+export const menuButtonsStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  borderRadius: '4px',
+  color: 'text.primary',
+  width: '180px',
+  '&:hover': {
+    backgroundColor: 'secondary.main',
+    border: 'none',
+    color: 'text.secondary',
+  },
+};
+
+export const menuIconsStyle = {
+  color: 'secondary.main',
+  minWidth: 0,
+  paddingRight: '10px',
+};
