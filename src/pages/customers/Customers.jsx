14d8777fe61814409979c42088ee88af361b6adc
Medina <mkazic1@etf.unsa.ba>
2023-01-27 21:59:26 +0100

    Implemented basic layout for sellers and customers

--- src/pages/customers/Customers.jsx
+++ src/pages/customers/Customers.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import { mainPagesLayoutStyle } from '../../styles/pages/PagesCommonStyle';
+import {
+  Box,
+  Button,
+  Typography,
+  Tooltip,
+} from '@mui/material';
+import { AddBox, Delete, Edit } from '@mui/icons-material';
+import {
+  titleBoxStyle,
+  mainPagesLayoutStyle,
+  mainPagesTitleStyle,
+  buttonsBoxStyle,
+} from '../../styles/pages/PagesCommonStyle';
 
 const Customers = () => (
   <Box style={mainPagesLayoutStyle}>
-    Customers
+    <Box sx={titleBoxStyle}>
+      <Typography variant="h6" sx={mainPagesTitleStyle}>Customers</Typography>
+      <Tooltip title="Create new invoice">
+        <Button
+          startIcon={<AddBox />}
+          size="medium"
+          variant="contained"
+          type="submit"
+        >
+          Create invoice
+        </Button>
+      </Tooltip>
+    </Box>
+    <Box sx={buttonsBoxStyle}>
+      <Tooltip title="Edit inactive invoice">
+        <Button
+          startIcon={<Edit />}
+          size="medium"
+          variant="contained"
+          sx={{ marginRight: '10px' }}
+        >
+          Edit
+        </Button>
+      </Tooltip>
+      <Tooltip title="Delete invoice">
+        <Button
+          startIcon={<Delete />}
+          size="medium"
+          variant="contained"
+        >
+          Delete
+        </Button>
+      </Tooltip>
+    </Box>
   </Box>
 );
 
--- src/pages/sellers/Sellers.jsx
+++ src/pages/sellers/Sellers.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import { mainPagesLayoutStyle } from '../../styles/pages/PagesCommonStyle';
+import {
+  Box,
+  Button,
+  Typography,
+  Tooltip,
+} from '@mui/material';
+import { AddBox, Delete, Edit } from '@mui/icons-material';
+import {
+  titleBoxStyle,
+  mainPagesLayoutStyle,
+  mainPagesTitleStyle,
+  buttonsBoxStyle,
+} from '../../styles/pages/PagesCommonStyle';
 
 const Sellers = () => (
   <Box style={mainPagesLayoutStyle}>
-    Sellers
+    <Box sx={titleBoxStyle}>
+      <Typography variant="h6" sx={mainPagesTitleStyle}>Sellers</Typography>
+      <Tooltip title="Create new invoice">
+        <Button
+          startIcon={<AddBox />}
+          size="medium"
+          variant="contained"
+          type="submit"
+        >
+          Create invoice
+        </Button>
+      </Tooltip>
+    </Box>
+    <Box sx={buttonsBoxStyle}>
+      <Tooltip title="Edit inactive invoice">
+        <Button
+          startIcon={<Edit />}
+          size="medium"
+          variant="contained"
+          sx={{ marginRight: '10px' }}
+        >
+          Edit
+        </Button>
+      </Tooltip>
+      <Tooltip title="Delete invoice">
+        <Button
+          startIcon={<Delete />}
+          size="medium"
+          variant="contained"
+        >
+          Delete
+        </Button>
+      </Tooltip>
+    </Box>
   </Box>
 );
 
