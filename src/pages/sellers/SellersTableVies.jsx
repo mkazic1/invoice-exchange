a0919c96bce48d750711c19dcc86df67e2da6880
Medina <mkazic1@etf.unsa.ba>
2023-01-27 22:36:28 +0100

    Table for customers and sellers

---
+++ src/pages/sellers/SellersTableVies.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from '@mui/material';
+import TABLE_HEADER from '../../constants-data/sellers-table';
+import headerCellStyle from '../../styles/pages/TableStyle';
+
+const SellersTableView = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Box sx={{ padding: '10px' }}>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 600 }}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                {TABLE_HEADER?.map((item) => (
+                  <TableCell key={item?.key} sx={headerCellStyle}>
+                    {item?.title}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow />
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10]}
+          component="div"
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+};
+
+export default SellersTableView;
