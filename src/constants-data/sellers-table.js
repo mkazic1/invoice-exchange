d6b97669c35cb1f56325664a2372915c8b1aac85
Medina <mkazic1@etf.unsa.ba>
2023-01-27 22:35:59 +0100

    Created file for table headers in sellers and customers

---
+++ src/constants-data/sellers-table.js
@@ -0,0 +1,16 @@
+const TABLE_HEADER = [
+  {
+    title: 'Company name',
+    key: 0,
+  },
+  {
+    title: 'Headquarters location',
+    key: 1,
+  },
+  {
+    title: 'Status',
+    key: 2,
+  },
+];
+
+export default TABLE_HEADER;
