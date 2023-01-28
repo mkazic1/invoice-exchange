2434bf4ae7ce52bf2d5967404cb292fe27c2e266
Medina <mkazic1@etf.unsa.ba>
2023-01-28 18:29:07 +0100

    Dashboard page simply implemented

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import { mainPagesLayoutStyle } from '../styles/pages/PagesCommonStyle';
+import { Box, Typography } from '@mui/material';
+import { mainPagesLayoutStyle, mainPagesTitleStyle, dashboardBoxStyle } from '../styles/pages/PagesCommonStyle';
 
 const DashboardPage = () => (
-  <Box sx={mainPagesLayoutStyle} />
+  <Box sx={mainPagesLayoutStyle}>
+    <Box sx={dashboardBoxStyle}>
+      <Typography variant="h4" sx={mainPagesTitleStyle}>
+        Welcome to Invoice Exchange System
+      </Typography>
+      <Typography variant="body1" sx={{ color: 'text.primary', textAlign: 'center' }}>
+        Welcome to your invoice management dashboard. Here, you can view your recent invoices,
+        manage your sellers and customers, and track your financials.
+      </Typography>
+    </Box>
+  </Box>
 );
 
 export default DashboardPage;
