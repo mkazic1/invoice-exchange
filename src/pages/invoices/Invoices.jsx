09fb74e93a980a503bbf4ca74f5471e86817a746
Medina <mkazic1@etf.unsa.ba>
2023-01-29 17:34:42 +0100

    Button box deleted from main component

--- src/pages/invoices/Invoices.jsx
+++ src/pages/invoices/Invoices.jsx
@@ -5,20 +5,17 @@ import {
   Typography,
   Tooltip,
 } from '@mui/material';
-import { AddBox, Delete, Edit } from '@mui/icons-material';
+import { AddBox } from '@mui/icons-material';
 import CreateInvoiceModal from './CreateInvoiceModal';
 import InvoicesTableView from './InvoicesTableView';
 import {
   titleBoxStyle,
   mainPagesLayoutStyle,
   mainPagesTitleStyle,
-  buttonsBoxStyle,
 } from '../../styles/pages/PagesCommonStyle';
-import DeleteModal from '../DeleteModal';
 
 const Invoices = () => {
   const [isCreateModalOpened, setIsCreateModalOpened] = useState(false);
-  const [isDeleteModalOpened, setIsDeleteModalOpened] = useState(false);
 
   return (
     <Box style={mainPagesLayoutStyle}>
@@ -38,32 +35,6 @@ const Invoices = () => {
         <CreateInvoiceModal
           isDialogOpened={isCreateModalOpened}
           setIsDialogOpened={setIsCreateModalOpened}
-        />
-      </Box>
-      <Box sx={buttonsBoxStyle}>
-        <Tooltip title="Edit inactive invoice">
-          <Button
-            startIcon={<Edit />}
-            size="medium"
-            variant="contained"
-            sx={{ marginRight: '10px' }}
-          >
-            Edit
-          </Button>
-        </Tooltip>
-        <Tooltip title="Delete invoice">
-          <Button
-            startIcon={<Delete />}
-            size="medium"
-            variant="contained"
-            onClick={() => setIsDeleteModalOpened(true)}
-          >
-            Delete
-          </Button>
-        </Tooltip>
-        <DeleteModal
-          isDialogOpened={isDeleteModalOpened}
-          setIsDialogOpened={setIsDeleteModalOpened}
         />
       </Box>
       <InvoicesTableView />
