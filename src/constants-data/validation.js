8726855e25a6f229916a8350709c1d793000b78e
Medina <mkazic1@etf.unsa.ba>
2023-01-30 04:08:16 +0100

    Added validation message

--- src/constants-data/validation.js
+++ src/constants-data/validation.js
@@ -3,6 +3,7 @@ const MESSAGES = {
   INVOICE_AMOUNT: 'Invoice amount must be more than 0',
   DATE: 'Date cannot be in the future',
   INVALID_INPUT: 'Invalid input',
+  INVALID_AMOUNT: 'Amount must be more than 0',
 };
 
 export default MESSAGES;
